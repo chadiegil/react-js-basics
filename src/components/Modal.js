0412dd67e6043de19bd56ad6923af3b1eae115cf
unknown <chadiegilaugis@gmail.com>
2022-03-20 11:13:03 +0800

    react basics

---
+++ src/components/Modal.js
@@ -0,0 +1,21 @@
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+  function confirmHandler() {
+    props.onConfirm();
+    alert("Confirm");
+  }
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        cancel
+      </button>
+      <button className="btn" onClick={confirmHandler}>
+        confirm
+      </button>
+    </div>
+  );
+}
+export default Modal;
